1666466789f31929bfc4f2a3e1bef1f7ae2b1e39
guiceroni <guiceroni@gmail.com>
2018-10-16 09:31:38 -0300

    Update tela.py

--- tela.py
+++ tela.py
@@ -29,7 +29,7 @@ class principal:
         self.bt_rec_cadastradas.place(x=450,y=150)
         self.bt_rec_return=Button(rec, text="Voltar", width=40, height=5, command= rec.destroy)
         self.bt_rec_return.place(x=250,y=350)   
-        rec.geometry("800x480")
+        rec.geometry("800x480+200+200")
         
     def receita_nova(self):
 
@@ -45,7 +45,7 @@ class principal:
         self.bt_confirma_recnova.place(x=150,y=300)
         self.bt_cancela_recnova=Button(rec_nova, text="Cancelar",background="red", width=30, height=5,command=rec_nova.destroy)
         self.bt_cancela_recnova.place(x=450,y=300)
-        rec_nova.geometry("800x480")
+        rec_nova.geometry("800x480+200+200")
 
     def conf_rampas(self):
     	conf_rampa=Tk()
@@ -53,13 +53,21 @@ class principal:
     		self.temp=Label(conf_rampa,text="Qual a temperatura?",font="Arial 15")
     		self.vtemp=ttk.Combobox(conf_rampa, width=20)
     		self.vtemp['values'] = ("40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76")
-    	conf_rampa.geometry("800x480")
+    	conf_rampa.geometry("800x480+200+200")
 
 
 root = Tk()
 principal(root)
 
-root.geometry("800x480")
+imagem = PhotoImage(file="TelaIHM.png")
+img = Label(root, image=imagem)
+img.imagem = imagem
+img.place(x=0, y=0)
+
+margem = Label(pri, height=7)
+margem.pack(anchor=W)
+
+root.geometry("800x480+200+200")
 root.mainloop()
 
 
