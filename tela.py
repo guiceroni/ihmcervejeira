a83fd5f446e2902e3977bc4b82b124cb3e9e7056
guiceroni <guiceroni@gmail.com>
2018-10-19 15:00:10 -0300

    Update tela.py

--- tela.py
+++ tela.py
@@ -41,6 +41,7 @@ class principal:
         self.wr_qtde_rampas=ttk.Combobox(rec_nova,width=20)
         self.wr_qtde_rampas['values'] = ("1","2","3","4","5","6","7")
         self.wr_qtde_rampas.place(x=400,y=190)
+              
         self.bt_confirma_recnova=Button(rec_nova, text="Confirmar", width=40, height=5,command=self.conf_rampas)
         self.bt_confirma_recnova.place(x=150,y=300)
         self.bt_cancela_recnova=Button(rec_nova, text="Cancelar", width=30, height=5,command=rec_nova.destroy)
@@ -48,34 +49,28 @@ class principal:
         rec_nova.geometry("800x480+200+200")
 
     def conf_rampas(self):
-    	conf_rampa=Tk()
-    	if self.wr_qtde_rampas == 1:
-    		self.temp=Label(conf_rampa,text="Qual a temperatura?",font="Arial 15")
-    		self.vtemp=ttk.Combobox(conf_rampa, width=20)
-    		self.vtemp['values'] = ("40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76")
-    	conf_rampa.geometry("800x480+200+200")
+    	conf_rampas=Tk()
+    	self.lb_conf_ramp=Label(conf_rampas, text="Cadastre sua receita", font="Arial 20")
+    	self.lb_conf_ramp.place(x=270, y=70)
+    	if lb_qtde_rampas == 1:
+            self.wr_escolhe_temp=ttk.Combobox(conf_rampas,width=20)
+            self.wr_escolhe_temp['values'] = ("1","2","3","4","5","6","7")
+            self.wr_escolhe_temp.place(x=400,y=190)   
+      	conf_rampas.geometry("800x480+200+200")
+
+    
 
 
 root = Tk()
 principal(root)
 
-imagem = PhotoImage(file="TelaIHM.png")
-img = Label(root, image=imagem)
-img.imagem = imagem
-img.place(x=0, y=0)
+#imagem = PhotoImage(file="TelaIHM.png")
+#img = Label(root, image=imagem)
+#img.imagem = imagem
+#img.place(x=0, y=0)
 
 margem = Label(root, height=7)
 margem.pack(anchor=W)
 
 root.geometry("800x480+200+200")
-root.mainloop()
-
-
-        
-
-
-
-
-
-
-
+root.mainloop()    
